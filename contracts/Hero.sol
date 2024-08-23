6afbed4050565d3b7b6604683b8ebe045a3b9623
benxin jiang <2795588099@qq.com>
2024-08-23 18:54:25 +0800

    game2

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -6,10 +6,55 @@ contract Hero {
         Healer,
         Barbarian
     }
-    function createHero(Class2 _class) public payable  {
+
+    mapping(address => uint[]) addressToHeroes;
+
+    function getHeroes() public view returns (uint[] memory) {
+        return addressToHeroes[msg.sender];
+    }
+    
+    function getStrength(uint hero) public pure returns(uint ) {
+        return (hero >> 2) & 0x1F;
+    }
+    function getHealth(uint hero) public pure returns(uint ) {
+        return (hero >> 7) & 0x1F;
+    }
+    function getIntellect(uint hero) public pure returns(uint ) {
+        return (hero >> 12) & 0x1F;
+    }
+    
+    function getMagic(uint hero) public pure returns(uint ) {
+        return (hero >> 17) & 0x1F;
+    }
+    function getDex(uint hero) public pure returns(uint ) {
+        return (hero >> 22) & 0x1F;
+    }
+    function generateRandom() public view virtual returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp,msg.sender)));
+    }
+// strength,health,intellect,magic,dexterity
+    function createHero(Class2 _class) public payable {
         require(
-            msg.value>=0.05 ether,
+            msg.value >= 0.05 ether,
             "Please send more money, at least 0.05 ether"
         );
+        uint[] memory stats = new uint[](5);
+        stats[0] = 2;
+        stats[1] = 7;
+        stats[2] = 12;
+        stats[3] = 17;
+        stats[4] = 22;
+
+        uint len = 5;
+        uint hero = uint(_class);
+        do{
+            uint pos = generateRandom() % len;
+            uint value = generateRandom() % (13 + len) + 1;
+            hero |= value << stats[pos];
+            len--;
+            stats[pos] = stats[len];
+        }while(len>0);
+
+        addressToHeroes[msg.sender].push(hero);
     }
 }
--- test/Hero.ts
+++ test/Hero.ts
@@ -4,7 +4,8 @@ import { expect } from "chai"
 
 describe('Hero', () => {
     async function createHero() {
-        const Hero = await ethers.getContractFactory("Hero")
+        // const Hero = await ethers.getContractFactory("Hero")
+        const Hero = await ethers.getContractFactory("TestHero")
         const hero = await Hero.deploy()
         await hero.waitForDeployment()
         return hero;
@@ -13,17 +14,33 @@ describe('Hero', () => {
     before(async function () {
         hero = await createHero()
     });
+
+    it("should get zero hero array", async function () {
+        expect(await hero.getHeroes()).to.deep.equal([])
+
+    })
+
     it("should fail at creating  hero because if paymeny", async function () {
         let e: any;
         try {
             await hero.createHero(0, {
-                value: ethers.parseEther("0.0499")
+                value: ethers.parseEther("0.0599")
             })
         } catch (_e) {
             e = _e
         }
-        console.log(e);
-        
         expect(e.message.includes("Please send more money, at least 0.05 ether")).to.equal(true)
+    })
+
+    it("should generate right stats value", async function () {
+        const hero: any = await createHero();
+        await hero.createHero(0, {
+            value: ethers.parseEther("0.05")
+        })
+        await hero.setRandom(69);
+        const h = (await hero.getHeroes())[0];
+        expect(await hero.getDex(h)).to.equal(16)
+        expect(await hero.getHealth(h)).to.equal(2)
+        expect(await hero.getStrenght(h)).to.equal(6)
     })
 })
